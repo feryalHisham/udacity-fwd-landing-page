69d1ca1d41c2615f6624f52ef19e2c6339ffa24f
Feryal Aboulenien <faboulenien@ejada.com>
2021-05-16 21:13:54 +0200

    Add creating navigation links logic.

--- js/app.js
+++ js/app.js
@@ -1,57 +1,31 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
 
 /**
- * Define Global Variables
+ * Any logic runs after the DOM content is loaded.
  * 
-*/
-
+ * main() function is the listener function for the `DOMContentLoaded` event which is the start point.
+ */
+document.addEventListener('DOMContentLoaded', main);
 
 /**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
+ * The main function which calls other functions
+ */
+function main () {
+    const sections = document.querySelectorAll('section');
+    const navigationMenu =  document.getElementById('navbar__list');
+    appendNavigationItemsToMenu(sections,navigationMenu);
+}
 
 /**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+ * Appends <a> elements (anchors) displaying the name of corresponding section and referencing the section id
+ * @param {*} sections 
+ * @param {*} navigationMenu 
+ */
+function appendNavigationItemsToMenu(sections, navigationMenu) {
+    for (const section of sections) {
+        const navigationLink = document.createElement('a');
+        navigationLink.href = '#' + section.id;
+        navigationLink.innerText = section.getAttribute('data-nav');
+        navigationLink.classList.add('menu__link');
+        navigationMenu.appendChild(navigationLink);
+    }
+}
